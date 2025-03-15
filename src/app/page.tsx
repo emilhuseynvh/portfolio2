90ce45ec3a7ff7fdfbbfd8a62af26893fe2cf2a1
emilhuseynvh <emilhuseynvh@gmail.com>
2025-03-15 14:16:19 +0400

    main page updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,10 +22,13 @@ const page: React.FC = () => {
         className={` ${rotate ? '!pt-0 !duration-700' : 'bg-primary-dark'} duration-1800 relative z-50`}
         style={{ paddingTop: `${padding}vh` }}
       >
-        <div className={`${rotate ? 'rotate-360 w-screen' : 'w-[50vw]'} h-screen bg-primary-bg duration-600 mx-auto`}>
+        <div className={`${rotate ? 'rotate-360 w-screen' : 'w-[50vw]'} bg-primary-bg duration-600 mx-auto`}>
           <div className="bg-paper"></div>
           <Header />
           <WorkSlider />
+          <div className='wrapper h1'>
+            <h1 className='text-[23vw] leading-[17vw] h1  text-primary-bg bg-primary-dark p-0 editorial-bold text-center'>APASNI</h1>
+          </div>
         </div>
       </div>
     </>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,7 +6,7 @@ const Header: React.FC = () => {
     return (
         <header className='flex w-full bg-primary-bg justify-between z-[52] inset-0 items-center wrapper h-[10.8vh] border-b-[1.5px] border-[#1d1d1b80]'>
             <div className='bg-paper h-full w-full z-40'></div>
-            <p className='tracking-[0.04em] text-[1.8vh] creator md:block hidden'>Amsterdam, NL</p>
+            <p className='tracking-[0.04em] text-[1.8vh] creator md:block hidden'>Azerbaijan, Baku</p>
             <div className='w-[19vh]'>
                 <Image className='w-full h-full' src={logo} alt='Logo' />
             </div>
